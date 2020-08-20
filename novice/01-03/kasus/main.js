8037d20199b3920f6c2a8b644194478d1f45f634
emanuelnormanpw <emanuelnpw@gmail.com>
2020-08-20 15:30:27 +0700

    update kasus

--- novice/01-03/kasus/main.js
+++ novice/01-03/kasus/main.js
@@ -1,40 +1,40 @@
-function convertCal(){
+function convertCal() {
     var valSatu = document.getElementById("valSatu").value;
     var jenisSatu = document.getElementById("pilVal").value;
 
-    var result="";
-    let status="";
+    var result = "";
+    let status = "";
 
-    if (jenisSatu=="CtoF"){
-        result = ((parseFloat(valSatu)*1.8)+32);
+    if (jenisSatu == "CtoF") {
+        result = ((parseFloat(valSatu) * 1.8) + 32);
         result = result.toFixed(2);
         status = "Celcius";
-    }else{
-        result = ((parseFloat(valSatu)-32)*0.56);
+    } else {
+        result = ((parseFloat(valSatu) - 32) * 0.56);
         parseInt(result);
         result = result.toFixed(2);
         status = "Farenheit";
     }
-    
-    document.getElementById("hasilConvert").innerHTML = result+" "+status;
+
+    document.getElementById("hasilConvert").innerHTML = result + " " + status;
 }
 
 
 
-function calLifetime(nilaiUsia, nilaiItem){
+function calLifetime(nilaiUsia, nilaiItem) {
     var nilaiUsia = document.getElementById("valLifeSatu").value;
-var nilaiItem = document.getElementById("valLifeDua").value;
+    var nilaiItem = document.getElementById("valLifeDua").value;
     const maxAge = 80;
     var result = "";
     var txt = "";
 
-    var itemTahun = nilaiItem*365;
+    var itemTahun = nilaiItem * 365;
     var sisaUmur = maxAge - nilaiUsia;
 
-    result = Math.floor(itemTahun)*sisaUmur;
+    result = Math.floor(itemTahun) * sisaUmur;
 
-    
-    txt = "You will need "+result+" to last you until the ripe old age of "+sisaUmur;
+
+    txt = "You will need " + result + " to last you until the ripe old age of " + sisaUmur;
 
     document.getElementById("hasilCalLifetime").innerHTML = txt;
 }
